6f92653e26a8011e5cdcdb06b880151a7a58e2d6
Misho Dzuliashvili <mishodzuliashvili1@gmail.com>
2024-06-09 23:07:13 +0400

    - make using Models global - change WeatherForecast Get function

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -19,9 +19,14 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<char> Get()
+    public IEnumerable<WeatherForecast> Get()
     {
-        return "Hello World!";
-
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
     }
 }
