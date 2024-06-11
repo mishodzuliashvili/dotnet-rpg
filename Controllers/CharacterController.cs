0c225735fd264df6bce69e287c4ae917755b3cdf
Misho Dzuliashvili <mishodzuliashvili1@gmail.com>
2024-06-12 01:09:29 +0400

    Add auto mapper

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,10 +10,6 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = [
-            new Character(),
-            new Character { Id = 1, Name = "Sam" }
-        ];
         private readonly ICharacterService _characterService;
 
         public CharacterController(ICharacterService characterService)
@@ -22,19 +18,19 @@ namespace dotnet_rpg.Controllers
         }
 
         [HttpGet("GetAll")]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
         {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{Id}")]
-        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int Id)
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int Id)
         {
             return Ok(await _characterService.GetCharacterById(Id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
         {
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
